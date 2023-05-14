168edb587c7337883674ad304c030733f04719d5
Dohyun-Ko <dohyun682@gmail.com>
2023-05-14 21:57:04 +0900

    add compile script and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to compile
+1. chmod +x compile.sh
+2. ./compile.h (or compile.h for windows)
+
---
+++ compile.sh
@@ -0,0 +1,7 @@
+emcc --bind -O3 \
+  components/element/Element.cpp \
+  components/button/Button.cpp \
+  components/flex/Flex.cpp \
+  components/input/Input.cpp \
+  index.cpp \
+  -o index.js
