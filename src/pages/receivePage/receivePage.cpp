80589ab5346af582d287d0bc8d9438ed71627d53
Dohyun-Ko <dohyun682@gmail.com>
2023-06-08 00:57:11 +0900

    receive page done

--- src/pages/receivePage/receivePage.cpp
+++ src/pages/receivePage/receivePage.cpp
@@ -1,19 +1,105 @@
 #include "receivePage.h"
+#include "../../components/style/Style.h"
+#include "../../components/state/State.cpp"
+#include "../../components/button/Button.h"
 
-ReceivePage* ReceivePage::instance = nullptr;
+ReceivePage *ReceivePage::instance = nullptr;
 
-ReceivePage::ReceivePage() : Element("div") {
+ReceivePage::ReceivePage() : Element("div")
+{
+    ReceivePage::getStyle()
+        .setWidth("100%")
+        .setHeight("100%")
+        .setDisplay("flex")
+        .setFlexDirection("row")
+        .setAlignItems("center")
+        .setJustifyContent("center");
+    prevButton = new Button(new State<string>("⬅"));
+    nextButton = new Button(new State<string>("➡"));
 
+    dutchItemContainer = new Element("div");
+    dutchItemContainer->getStyle()
+        .setWidth("70%")
+        .setHeight("50%")
+        .setDisplay("grid")
+        .setGridTemplateColumns("repeat(3, 1fr)")
+        .setGap("25px");
+
+    dutchItemStyle = new Style();
+    dutchItemStyle->setWidth("100%")
+        .setDisplay("flex")
+        .setFlexDirection("column")
+        .setAlignItems("center")
+        .setJustifyContent("space-between")
+        .setBackground(Style::secondaryVariant)
+        .setBorder("none")
+        .setBorderRadius("8px")
+        .setFontSize("20px")
+        .setFontWeight("bold")
+        .setColor(Style::secondaryVariant)
+        .setAspectRatio("1.618")
+        .setPadding("10px 0")
+        .setColor("white");
+
+    dutchItemUserNameStyle = new Style();
+    dutchItemUserNameStyle->setFontSize("12px")
+        .setFontWeight("600")
+        .setWidth("calc(100\% - 20px)")
+        .setTextAlign("left")
+        .setBackground(Style::primary)
+        .setPadding("10px 10px 0px 10px")
+        .setMargin("0")
+        .setHeight("20%");
+
+    dutchItemChargeStyle = new Style();
+    dutchItemChargeStyle->setFontSize("16px")
+        .setFontWeight("400")
+        .setWidth("calc(100\% - 20px)")
+        .setTextAlign("right")
+        .setPadding("0px 10px 10px 10px")
+        .setMargin("0")
+        .setHeight("20%");
+
+    dutchItem1 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem1->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItem2 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem2->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItem3 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem3->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItem4 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem4->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItem5 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem5->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItem6 = new Button(new State<string>("Dutch to"), dutchItemStyle);
+    dutchItem6->appendChildren({new Text(new State<string>("User Name, User Name"), dutchItemUserNameStyle),
+                                new Text(new State<string>("$ 50 / 100"), dutchItemChargeStyle)});
+
+    dutchItemContainer->appendChildren({dutchItem1, dutchItem2, dutchItem3, dutchItem4, dutchItem5, dutchItem6});
+
+    ReceivePage::appendChildren({prevButton, dutchItemContainer, nextButton});
 }
 
-ReceivePage* ReceivePage::getInstance() {
-    if (ReceivePage::instance == nullptr) {
+ReceivePage *ReceivePage::getInstance()
+{
+    if (ReceivePage::instance == nullptr)
+    {
         ReceivePage::instance = new ReceivePage();
     }
 
     return ReceivePage::instance;
 }
 
-ReceivePage::~ReceivePage() {
+ReceivePage::~ReceivePage()
+{
     delete instance;
 }
--- src/pages/receivePage/receivePage.h
+++ src/pages/receivePage/receivePage.h
@@ -7,20 +7,38 @@
 #include "../../components/flex/Flex.h"
 #include "../../components/text/Text.h"
 
-class ReceivePage : public Element {
+class ReceivePage : public Element
+{
 
-    Button* receiveDutchButtons[8]; //받을 수 있는 더치 리스트 (event의 target을 이용해서 더치 uuid를 가져올 수 있도록 로직 구성예정)
-    Button* nextButton; //8개 이상의 더치가 있을 경우 다음 페이지로 넘어가는 버튼
-    Button* prevButton; //8개 이상의 더치가 있을 경우 이전 페이지로 넘어가는 버튼
+    Button *receiveDutchButtons[8]; // 받을 수 있는 더치 리스트 (event의 target을 이용해서 더치 uuid를 가져올 수 있도록 로직 구성예정)
+    Button *nextButton;             // 8개 이상의 더치가 있을 경우 다음 페이지로 넘어가는 버튼
+    Button *prevButton;             // 8개 이상의 더치가 있을 경우 이전 페이지로 넘어가는 버튼
 
-    static ReceivePage* instance;
+    State<int> *currentPage = 0;
+
+    Style *dutchItemStyle;
+    Style *dutchItemUserNameStyle;
+    Style *dutchItemChargeStyle;
+
+    Button *dutchItem1;
+    Button *dutchItem2;
+    Button *dutchItem3;
+    Button *dutchItem4;
+    Button *dutchItem5;
+    Button *dutchItem6;
+    Button *dutchItem7;
+    Button *dutchItem8;
+
+    Element *dutchItemContainer;
+
+    static ReceivePage *instance;
     ReceivePage();
 
-    public:
-        ~ReceivePage();
+public:
+    ~ReceivePage();
 
-        static ReceivePage* getInstance();
-        static void nextButtonHandler(emscripten::val event);
-        static void prevButtonHandler(emscripten::val event);
-        static void receiveDutchButtonHandler(emscripten::val event);
+    static ReceivePage *getInstance();
+    static void nextButtonHandler(emscripten::val event);
+    static void prevButtonHandler(emscripten::val event);
+    static void receiveDutchButtonHandler(emscripten::val event);
 };
